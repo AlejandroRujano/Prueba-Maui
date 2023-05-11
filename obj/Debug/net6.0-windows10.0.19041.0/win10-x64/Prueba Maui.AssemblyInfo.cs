2e4ccb705f35046aed86ab7f551979e43f674d75
Lukar10Y <lukariXU01@gmail.com>
2023-05-10 22:25:56 -0400

    Subiendo 20 cambios que me salieron luego de clonar

--- obj/Debug/net6.0-windows10.0.19041.0/win10-x64/Prueba Maui.AssemblyInfo.cs
+++ obj/Debug/net6.0-windows10.0.19041.0/win10-x64/Prueba Maui.AssemblyInfo.cs
@@ -11,14 +11,10 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.PackageName", "87797024-6358-4fa6-a8b7-4fbb5b5cefff")]
-[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.PublisherName", "User Name")]
-[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.Name", "Prueba Maui")]
-[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.Version", "1.0.0.1")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Prueba Maui")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Prueba Maui")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Prueba Maui")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
